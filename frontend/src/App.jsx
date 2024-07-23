23839172b68d8acd06cfc31095450ea1367ad11f
UJJWAL <Ujjwal@gmail.com>
2024-07-23 06:28:35 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,23 @@
+import lease from "./lease.png"
+import notebook from "./notebook.png"
+import shoppingcart from "./shoppingcart.png"
+import support from "./support.png"
+import menu from "./menu.png"
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
+export const assets ={
+    lease,
+    notebook,
+    shoppingcart,
+    support,
+    menu
+ } 
+ export const imagessss ={
+    image1,
+    image2,
+    image3,
+    image4
+ } 
+ 
