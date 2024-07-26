52117e8584d929ca3e65e97d201a5af7012a692f
UJJWAL <Ujjwal@gmail.com>
2024-07-26 06:46:48 +0530

    7bje soneka  commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { FaSearch, FaUser } from 'react-icons/fa'; // Import the search and user
 const Header = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [profileSidebarOpen, setProfileSidebarOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('/'); // State to track the active link
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -12,6 +13,12 @@ const Header = () => {
 
   const toggleProfileSidebar = () => {
     setProfileSidebarOpen(!profileSidebarOpen);
+  };
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path); // Set the active link
+    setSidebarOpen(false); // Close the sidebar on link click
+    setProfileSidebarOpen(false); // Close the profile sidebar on link click
   };
 
   return (
@@ -42,13 +49,62 @@ const Header = () => {
 
       {/* Desktop Navigation */}
       <nav className="hidden space-x-4 md:flex">
-        <Link to="/" className="hover:text-gray-400">Home</Link>
-        <Link to="/yourgames" className="hover:text-gray-400">YourGames</Link>
-        <Link to="/rewards" className="hover:text-gray-400">Rewards</Link>
-        <Link to="/achievements" className="hover:text-gray-400">Achievements</Link>
-        <Link to="/about" className="hover:text-gray-400">About</Link>
-        <Link to="/contact" className="hover:text-gray-400">Contact</Link>
-        <Link to="/privacypolicy" className="hover:text-gray-400">PrivacyPolicy</Link>
+        <Link
+          to="/"
+          className={`relative ${activeLink === '/' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/')}
+        >
+          Home
+          {activeLink === '/' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/yourgames"
+          className={`relative ${activeLink === '/yourgames' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/yourgames')}
+        >
+          Your Games
+          {activeLink === '/yourgames' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/rewards"
+          className={`relative ${activeLink === '/rewards' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/rewards')}
+        >
+          Rewards
+          {activeLink === '/rewards' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/achievements"
+          className={`relative ${activeLink === '/achievements' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/achievements')}
+        >
+          Achievements
+          {activeLink === '/achievements' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/about"
+          className={`relative ${activeLink === '/about' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/about')}
+        >
+          About
+          {activeLink === '/about' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/contact"
+          className={`relative ${activeLink === '/contact' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/contact')}
+        >
+          Contact
+          {activeLink === '/contact' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
+        <Link
+          to="/privacypolicy"
+          className={`relative ${activeLink === '/privacypolicy' ? 'text-blue-400' : 'hover:text-gray-400'}`}
+          onClick={() => handleLinkClick('/privacypolicy')}
+        >
+          Privacy Policy
+          {activeLink === '/privacypolicy' && <span className="absolute inset-x-0 bottom-0 block border-b-2 border-blue-400"></span>}
+        </Link>
         <button className="px-4 py-2 font-bold text-white transition duration-300 transform rounded-full shadow-lg bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
           <Link to="/login" className="hover:text-gray-400">Login/SignIn</Link>
         </button>
@@ -66,16 +122,16 @@ const Header = () => {
           </button>
         </div>
         <nav className="flex flex-col items-center mt-10 space-y-4">
-          <Link to="/" className="text-lg text-white" onClick={toggleSidebar}>Home</Link>
-          <Link to="/games" className="text-lg text-white" onClick={toggleSidebar}>Games</Link>
-          <Link to="/about" className="text-lg text-white" onClick={toggleSidebar}>About</Link>
-          <Link to="/contact" className="text-lg text-white" onClick={toggleSidebar}>Contact</Link>
-          <Link to="/privacypolicy" className="text-lg text-white" onClick={toggleSidebar}>PrivacyPolicy</Link>
-          <Link to="/settings" className="text-lg text-white" onClick={toggleSidebar}>Settings</Link>
+          <Link to="/" className={`text-lg text-white ${activeLink === '/' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/')}>Home</Link>
+          <Link to="/yourgames" className={`text-lg text-white ${activeLink === '/yourgames' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/yourgames')}>Your Games</Link>
+          <Link to="/rewards" className={`text-lg text-white ${activeLink === '/rewards' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/rewards')}>Rewards</Link>
+          <Link to="/achievements" className={`text-lg text-white ${activeLink === '/achievements' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/achievements')}>Achievements</Link>
+          <Link to="/about" className={`text-lg text-white ${activeLink === '/about' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/about')}>About</Link>
+          <Link to="/contact" className={`text-lg text-white ${activeLink === '/contact' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/contact')}>Contact</Link>
+          <Link to="/privacypolicy" className={`text-lg text-white ${activeLink === '/privacypolicy' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/privacypolicy')}>Privacy Policy</Link>
           <button className="px-4 py-2 font-bold text-white transition duration-300 transform rounded-full shadow-lg bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
-            <Link to="/login" className="text-lg text-white" onClick={toggleSidebar}>Login/SignIn</Link>
+            <Link to="/login" className="text-lg text-white">Login/SignIn</Link>
           </button>
-          
         </nav>
       </div>
 
@@ -91,10 +147,13 @@ const Header = () => {
           </button>
         </div>
         <nav className="flex flex-col items-center mt-10 space-y-4">
-        <Link to="/" className="text-lg text-white" onClick={toggleProfileSidebar}>Home</Link>
-          <Link to="/yourgames" className="text-lg text-white" onClick={toggleProfileSidebar}>Your Games</Link>
-          <Link to="/rewards" className="text-lg text-white" onClick={toggleProfileSidebar}>Rewards</Link>
-          <Link to="/achievements" className="text-lg text-white" onClick={toggleProfileSidebar}>Achievements</Link>
+          <Link to="/" className={`text-lg text-white ${activeLink === '/' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/')}>Home</Link>
+          <Link to="/yourgames" className={`text-lg text-white ${activeLink === '/yourgames' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/yourgames')}>Your Games</Link>
+          <Link to="/rewards" className={`text-lg text-white ${activeLink === '/rewards' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/rewards')}>Rewards</Link>
+          <Link to="/achievements" className={`text-lg text-white ${activeLink === '/achievements' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/achievements')}>Achievements</Link>
+          <Link to="/about" className={`text-lg text-white ${activeLink === '/about' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/about')}>About</Link>
+          <Link to="/contact" className={`text-lg text-white ${activeLink === '/contact' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/contact')}>Contact</Link>
+          <Link to="/privacypolicy" className={`text-lg text-white ${activeLink === '/privacypolicy' ? 'text-blue-400' : ''}`} onClick={() => handleLinkClick('/privacypolicy')}>Privacy Policy</Link>
           <button className="px-4 py-2 font-bold text-white transition duration-300 transform rounded-full shadow-lg bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
             <Link to="/logout" className="text-lg text-white" onClick={() => { /* Add logout logic */ toggleProfileSidebar(); }}>LogOut</Link>
           </button>
